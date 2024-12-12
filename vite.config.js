170cd8fb51964c8bb255db2a43731f9c3f28124d
Abiqb93 <ab.iqb93@gmail,com>
2024-12-12 15:24:45 +0500

    Final Front End 3

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: [{ find: "@", replacement: "/src" }],
-//   },
-//   base: '/horses-website-deployed/', // Add this line to set the correct base path for GitHub Pages
-// });
-
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
-  base: "/", // Ensure the root path is set
-  server: {
-    port: 8080, // Local development port
-    proxy: {
-      '/api': {
-        target: 'https://horseracesbackend-production.up.railway.app/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: '/horses-website-deployed/', // Add this line to set the correct base path for GitHub Pages
 });
